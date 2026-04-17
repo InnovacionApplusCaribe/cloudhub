c0e5e1dad00188fc7a68e3ea58ba73aa2d8c9d2b
Johat Edilberto Abrego Nuñez <johat.abrego@applusglobal.com>
2026-04-17 10:52:08 -0500

    add linux PotreeConverter binary

--- server/config/index.js
+++ server/config/index.js
@@ -2,14 +2,18 @@ const path = require('path');
 require('dotenv').config();
 const { BlobServiceClient } = require('@azure/storage-blob');
 
+const isWin = process.platform === 'win32';
+
 const config = {
     port: process.env.PORT || 3000,
-    potreeConverterPath: process.env.POTREE_CONVERTER_PATH || path.join(__dirname, '../../PotreeConverter/PotreeConverter.exe'),
+    potreeConverterPath: process.env.POTREE_CONVERTER_PATH || (isWin
+        ? path.join(__dirname, '../../PotreeConverter/PotreeConverter.exe')
+        : path.join(__dirname, '../../PotreeConverter/linux/PotreeConverter')),
     uploadsDir: path.join(__dirname, '../../data/uploads'),
     convertedDir: path.join(__dirname, '../../data/converted'),
     tempCloudDir: path.join(__dirname, '../../data/temp'),
     azureProjectsFile: path.join(__dirname, '../../data/azure_projects.json'),
-    
+
     azure: {
         connectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
         rawContainer: process.env.AZURE_RAW_CONTAINER_NAME || 'raw-las',
